66cb2f4811097033d355ed5e5f1803c635e6cfce
Sam Bigelow <sambigelow44@gmail.com>
2021-07-18 14:56:08 -0400

    Add some styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,49 +1,50 @@
-const moment = require("moment");
+const moment = require('moment')
 
-moment.locale("en");
+moment.locale('en')
 
 module.exports = function config(eleventyConfig) {
-  eleventyConfig.addFilter("dateIso", (date) => moment(date).toISOString());
+  eleventyConfig.addFilter('dateIso', (date) => moment(date).toISOString())
 
-  eleventyConfig.addFilter("dateReadable", (date) =>
-    moment(date).utc().format("LL")
-  );
+  eleventyConfig.addFilter('dateReadable', (date) =>
+    moment(date).utc().format('LL')
+  )
 
-  global.filters = eleventyConfig.javascriptFunctions;
+  global.filters = eleventyConfig.javascriptFunctions
   eleventyConfig.setPugOptions({
-    globals: ["filters"],
-  });
+    globals: ['filters'],
+  })
 
-  eleventyConfig.addShortcode("excerpt", (article) => extractExcerpt(article));
-};
+  eleventyConfig.addShortcode('excerpt', (article) => extractExcerpt(article))
+  eleventyConfig.addPassthroughCopy('css')
+}
 
 function extractExcerpt(article) {
-  if (!article.hasOwnProperty("templateContent")) {
+  if (!article.hasOwnProperty('templateContent')) {
     console.warn(
       'Failed to extract excerpt: Document has no property "templateContent".'
-    );
-    return null;
+    )
+    return null
   }
 
-  const content = article.templateContent;
+  const content = article.templateContent
 
   const separatorsList = [
-    { start: "<!-- Excerpt Start -->", end: "<!-- Excerpt End -->" },
-    { start: "<p>", end: "</p>" },
-  ];
+    { start: '<!-- Excerpt Start -->', end: '<!-- Excerpt End -->' },
+    { start: '<p>', end: '</p>' },
+  ]
 
-  let excerpt;
+  let excerpt
   separatorsList.some((separators) => {
-    const startPosition = content.indexOf(separators.start);
-    const endPosition = content.indexOf(separators.end);
+    const startPosition = content.indexOf(separators.start)
+    const endPosition = content.indexOf(separators.end)
 
     if (startPosition !== -1 && endPosition !== -1) {
       excerpt = content
         .substring(startPosition + separators.start.length, endPosition)
-        .trim();
-      return true;
+        .trim()
+      return true
     }
-  });
+  })
 
-  return excerpt;
+  return excerpt
 }
